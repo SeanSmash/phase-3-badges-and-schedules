763e6d514300bacdaf55fa070af9a08c8646c950
Sean Sarokas <sps4462@gmail.com>
2022-12-22 08:19:24 -0500

    commit tree 9e2d37c61bc60d7e22d79a0709d841fdb7be4e81

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
+require 'pry'
+
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map {|n| "Hello, my name is #{n}."}
+end
+
+def assign_rooms(names)
+    room_and_welcome = []
+    names.each_with_index do |name, index|
+        room_and_welcome << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    room_and_welcome
+end
+
+def printer(names)
+    badges = batch_badge_creator(names)
+    badges.each {|n| puts n}
+    rooms = assign_rooms(names)
+    rooms.each {|n| puts n}
+end
